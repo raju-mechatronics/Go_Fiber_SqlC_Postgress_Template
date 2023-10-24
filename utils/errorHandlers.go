a734f5ab1a0a3ae092d47bf3404bdf7adb9f20a7
Raju Ahmed <rajuahmed1708@gmail.com>
2023-10-24 08:49:59 +0600

    added some error handling utility

--- utils/errorHandlers.go
+++ utils/errorHandlers.go
@@ -13,3 +13,17 @@ func FetalError(err error) {
 		log.Fatal(err)
 	}
 }
+
+func HandleFunctionPanic[T any](t T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+func HandleFunctionFetal[T any](t T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
